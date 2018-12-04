385425f1452d625dd9da21813aee1a2c23c5e18d
renda <122700138@qq.com>
2018-12-05 00:05:10 +0800

    n

--- renda/base/Date.cpp
+++ renda/base/Date.cpp
@@ -2,13 +2,12 @@
 // Created by rhp on 18-12-4.
 //
 
-#include <stdint-gcc.h>
 #include "Date.h"
 namespace renda
 {
 namespace details
 {
-char require_32_bit_int_at_least[sizeof(int) >= sizeof(int32_t) ? 1:-1];
+char require_32_bit_int_at_least[sizeof(int) >= sizeof(int32_t) ? 1 : -1];
 
 int getJulianDayNumber(int year, int month, int day)
 {
@@ -16,7 +15,7 @@ int getJulianDayNumber(int year, int month, int day)
     int a = (14 - month) / 12;
     int y = year + 4800 - a;
     int m = month + 12 * a - 3;
-    return day + (153*m + 2) / 5 + 365*y + y/4 - y/100 + y/400 - 32045;
+    return day + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045;
 }
 
 Date::YearMonthDay getYearMonthDay(int julianDayNum)
@@ -33,13 +32,38 @@ Date::YearMonthDay getYearMonthDay(int julianDayNum)
     ymd.year = b * 100 + d - 4800 + (m / 10);
     return ymd;
 }
-}
+} // namespace details
 
-static const int Date::kJulianDayof1970_01_01 = details::getJulianDayNumber(1970, 1, 1);
+const int Date::kJulianDayof1970_01_01 = details::getJulianDayNumber(1970, 0, 0);
 
-YearMonthDay Date::yearMonthDay() const
+} // namespace renda
+
+using namespace renda;
+using namespace renda::details;
+
+Date::Date(int y, int m, int d)
+    : julianDayNum_(getJulianDayNumber(y, m, d))
 {
+}
 
+Date::Date(const tm &t)
+    : julianDayNum_(getJulianDayNumber(
+          t.tm_year + 1900,
+          t.tm_mon + 1,
+          t.tm_mday))
+{
 }
+
+string Date::toIsoString() const
+{
+    char buf[32];
+    YearMonthDay ymd(yearMonthDay());
+    snprintf(buf, sizeof(buf) - 1, "%4d-%2d-%2d",
+        ymd.year, ymd.month, ymd.day);
+    return buf;
 }
+
+Date::YearMonthDay Date::yearMonthDay() const
+{
+    return getYearMonthDay(julianDayNum_);
 }
--- renda/base/Date.h
+++ renda/base/Date.h
@@ -5,91 +5,91 @@
 #ifndef RENDA_DATE_H
 #define RENDA_DATE_H
 
-#include "types.h"
 #include "copyable.h"
+#include "types.h"
 namespace renda
 {
 
-
-    class Date : public copyable
+class Date : public copyable
+{
+  public:
+    struct YearMonthDay
     {
-    public:
-        struct YearMonthDay
-        {
-            int year;
-            int month;
-            int day;
-        };
-
-        static const int kDaysPerWeek = 7;
-        static const int kJulianDayof1970_01_01;
-
-        Date()
-        : julianDayNum_(0)
-        {}
-
-        Date(int year, int month, int day);
-
-        explicit Date(int julianDay)
-        : julianDayNum_(julianDay)
-        {}
-
-        explicit Date(const tm);
-
-        void swap(Date& that)
-        {
-            std::swap(julianDayNum_, that.julianDayNum_);
-        }
-
-        bool valid()
-        {
-            return julianDayNum_ > 0;
-        }
-
-        string toIsoString() const;
-
-        YearMonthDay yearMonthDay() const;
-
-        int year() const
-        {
-            return yearMonthDay().year;
-        }
-
-        int month() const
-        {
-            return yearMonthDay().month;
-        }
-
-        int day() const
-        {
-            return yearMonthDay().day;
-        }
-
-        int weekDay() const
-        {
-            return (julianDayNum_ + 1) % kDaysPerWeek;
-        }
-
-        int julianDayNumber() const
-        {
-            return julianDayNum_;
-        }
-
-    private:
-        int julianDayNum_;
-
+        int year;
+        int month;
+        int day;
     };
 
-inline bool operator < (Date& x, Date& y)
+    static const int kDaysPerWeek = 7;
+    static const int kJulianDayof1970_01_01;
+
+    Date()
+        : julianDayNum_(0)
+    {
+    }
+
+    Date(int year, int month, int day);
+
+    explicit Date(int julianDay)
+        : julianDayNum_(julianDay)
+    {
+    }
+
+    explicit Date(const tm &);
+
+    void swap(Date &that)
+    {
+        std::swap(julianDayNum_, that.julianDayNum_);
+    }
+
+    bool valid()
+    {
+        return julianDayNum_ > 0;
+    }
+
+    string toIsoString() const;
+
+    YearMonthDay yearMonthDay() const;
+
+    int year() const
+    {
+        return yearMonthDay().year;
+    }
+
+    int month() const
+    {
+        return yearMonthDay().month;
+    }
+
+    int day() const
+    {
+        return yearMonthDay().day;
+    }
+
+    int weekDay() const
+    {
+        return (julianDayNum_ + 1) % kDaysPerWeek;
+    }
+
+    int julianDayNumber() const
+    {
+        return julianDayNum_;
+    }
+
+  private:
+    int julianDayNum_;
+};
+
+inline bool operator<(Date &x, Date &y)
 {
     return x.julianDayNumber() < y.julianDayNumber();
 }
 
-inline bool operator == (Date& x, Date& y)
+inline bool operator==(Date &x, Date &y)
 {
     return x.julianDayNumber() == y.julianDayNumber();
 }
 
-}
+} // namespace renda
 
 #endif //RENDA_DATE_H
